e842acac9b8c30efd03edeaaa05b6a2fb08930cb
Brian Anderson <Brian Anderson>
2016-08-07 12:49:41 +0300

    Completed the controller and routes for Region, District,Metadata, Categories, Metadata_Category Models.

---
+++ app/Http/Controllers/Data_ValuesController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class Data_ValuesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data_value = new Data_Value;
+
+        $data_value->arrivedData = $request->arrivedData;
+
+        $data_value->save();
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data_value = App\Data_Values::find(1);
+
+        //Attributes to be updated.
+
+        $data_value->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,22 @@ Route::get('home', function () {
     return view('welcome');
 });
 
+Route::resource('metadata', 'MetadataController', ['only' => ['store']]);
+Route::post('metadata/{id}', 'MetadataController@store');
+
+Route::resource('categories', 'CategoriesController', ['only' => ['store']]);
+Route::post('categories/{id}', 'CategoriesController@store');
+
+Route::resource('metadata_category', 'Metadata_CategoryController', ['only' => ['store']]);
+Route::post('metadata_category/{id}', 'Metadata_CategoryController@store');
+
+Route::resource('district', 'DistrictController', ['only' => ['store']]);
+Route::post('district/{id}', 'DistrictController@store');
+
+Route::resource('region', 'RegionController', ['only' => ['store']]);
+Route::post('region/{id}', 'RegionController@store');
+
+Route::resource('data_value', 'Data_ValuesController', ['except' => ['index', 'create', 'edit']]);
+Route::post('data_value/{id}', 'Data_ValuesController@store');
+Route::put('data_value/{id}', 'Data_ValuesController@update');
+Route::get('data_value/{id}', 'Data_ValuesController@show');
